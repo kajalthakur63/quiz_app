f7ebfed9b3ae17e400dc64fa474c044cc89e5df1
kajalthakur63 <154333088+kajalthakur63@users.noreply.github.com>
2023-12-19 17:40:32 +0530

    Add files via upload

---
+++ alt_text.dart
@@ -0,0 +1,226 @@
+Map WELCOME={
+  'eng': 'Welcome to the Quiz!',
+  'hindi': 'प्रश्नोत्तरी में आपका स्वागत है!'
+};
+
+Map START={
+  'eng':'Start quiz',
+  'hindi':'प्रश्नोत्तरी प्रारंभ करें'
+};
+Map Qus1 ={
+  'eng': 'What is Flutter?',
+  'hindi': 'स्पंदन क्या है?'
+};
+Map Option1={
+  'eng': 'Flutter is an open-source UI toolkit',
+   'hindi':'फ़्लटर एक ओपन-सोर्स यूआई टूलकिट है',
+};
+
+Map Option2={
+'eng': ' Flutter is an open-source backend development framework',
+'hindi':'फ़्लटर एक ओपन-सोर्स बैकएंड डेवलपमेंट फ्रेमवर्क है',
+};
+
+Map Option3={
+'eng':'Flutter is an open-source programming language for cross-platform applications',
+'hindi':'फ़्लटर क्रॉस-प्लेटफ़ॉर्म अनुप्रयोगों के लिए एक ओपन-सोर्स प्रोग्रामिंग भाषा है',
+};
+
+Map Option4={
+'eng': ' Flutters is a DBMS toolkit',
+'hindi': 'फ़्लटर एक DBMS टूलकिट है',
+};
+
+Map Qus2={
+'eng':'Who developed the Flutter Framework and continues to maintain it today?',
+'hindi': 'फ़्लटर फ़्रेमवर्क किसने विकसित किया और आज भी इसका रखरखाव कर रहा है?'
+};
+Map Qus2Option1={
+  'eng':'Google',
+  'hindi':'गूगल',
+};
+
+Map Qus2Option2={
+'eng':'Facebook',
+'hindi':'फेसबुक',
+};
+
+Map Qus2Option3={
+'eng':  ' Microsoft',
+  'hindi':'माइक्रोसॉफ्ट',
+};
+
+Map Qus2Option4= {
+'eng': 'Oracle',
+'hindi':'आकाशवाणी'
+};
+
+Map Qus3={
+  'eng':'Which programming language is used to build Flutter applications?',
+  'hindi':'फ़्लटर एप्लिकेशन बनाने के लिए किस प्रोग्रामिंग भाषा का उपयोग किया जाता है?',
+};
+Map Qus3Option1={
+'eng':'Dart',
+'hindi':'डार्ट',
+};
+Map Qus3Option2={
+'eng':'Kotlin',
+'hindi':'कोटलिन',
+};
+Map Qus3Option3={
+'eng':'Java',
+'hindi':'जावा',
+};
+Map Qus3Option4={
+'eng':'Go',
+'hindi': 'गो'
+};
+
+Map Qus4={
+  'eng':'How many types of widgets are there in Flutter?',
+  'hindi':'फ़्लटर में कितने प्रकार के विजेट हैं?',
+};
+Map Qus5={
+  'eng':'When building for iOS, Flutter is restricted to an __ compilation strategy',
+  'hindi':'आईओएस के लिए निर्माण करते समय, फ़्लटर एक __ संकलन रणनीति तक सीमित है',
+};
+Map Qus5Option1={
+  'eng':'AOT (ahead-of-time)',
+  'hindi':'एओटी (समय से पहले)',
+};
+Map Qus5Option2={
+  'eng':'JIT (Just-in-time)',
+  'hindi':'जेआईटी (जस्ट-इन-टाइम)',
+};
+Map Qus5Option3={
+  'eng': 'Transcompilation',
+  'hindi':'ट्रांसकंपाइलेशन',
+};
+Map Qus5Option4={
+  'eng': 'Recompilation',
+  'hindi':'रीकंपाइलेशन',
+};
+Map Qus6={
+  'eng':'A sequence of asynchronous Flutter events is known as a:',
+  'hindi':'अतुल्यकालिक स्पंदन घटनाओं के अनुक्रम को इस रूप में जाना जाता है:',
+};
+Map Qus6Option1={
+  'eng':'Stream',
+  'hindi':'स्ट्रीम',
+};
+Map Qus6Option2={
+  'eng':'Flow',
+  'hindi':'फ्लो',
+};
+Map Qus6Option3={
+  'eng':'Current',
+  'hindi': 'करंट',
+};
+Map Qus6Option4={
+  'eng':'Series',
+  'hindi':'सीरीज़',
+};
+Map Qus7={
+  'eng':'Access to a cloud database through Flutter is available through which service?',
+  'hindi':'फ़्लटर के माध्यम से क्लाउड डेटाबेस तक पहुंच किस सेवा के माध्यम से उपलब्ध है?',
+};
+Map Qus7Option1={
+  'eng':'Firebase Database',
+  'hindi':'फ़ायरबेस डेटाबेस',
+
+
+};
+Map Qus7Option2={
+  'eng':'SQLite',
+  'hindi':'SQLite',
+
+};
+Map Qus7Option3={
+'eng': 'NOSQL',
+  'hindi': 'NOSQL',
+};
+Map Qus7Option4={
+  'eng':'MYSQL',
+  'hindi':'MYSQL',
+
+};
+
+Map Qus8={
+  'eng':'What are some key advantages of Flutter over alternate frameworks?',
+  'hindi':'वैकल्पिक ढाँचों की तुलना में फ़्लटर के कुछ प्रमुख लाभ क्या हैं?',
+};
+Map Qus8Option1={
+  'eng': 'All of the above',
+  'hindi':'उपरोक्त सभी',
+};
+Map Qus8Option2={
+  'hindi': 'रैपिड क्रॉस-प्लेटफॉर्म एप्लिकेशन डेवलपमेंट और डिबगिंग टूल्स',
+  'eng': 'Rapid cross-platform application development and debugging tools',
+};
+Map Qus8Option3={
+  'eng':'Future-proofed technologies and UI resources',
+  'hindi':'फ्यूचर-प्रूफ़्ड टेक्नोलॉजीज और यूआई संसाधन',
+};
+Map Qus8Option4={
+  'eng':'Strong supporting tools for application development and launch',
+  'hindi': 'एप्लिकेशन विकास और लॉन्च के लिए मजबूत सहायक उपकरण',
+};
+
+Map Qus9={
+  'eng':'What element is used as an identifier for components when programming in Flutter?',
+  'hindi':'फ़्लटर में प्रोग्रामिंग करते समय घटकों के लिए पहचानकर्ता के रूप में किस तत्व का उपयोग किया जाता है?',
+};
+Map Qus9Option1={
+  'eng':'Keys',
+  'hindi':'कुंजियाँ',
+};
+Map Qus9Option2={
+  'eng':'Widgets',
+  'hindi':'विजेट्स',
+};
+Map Qus9Option3={
+  'eng':'Elements',
+  'hindi': 'तत्व',
+};
+Map Qus9Option4={
+  'eng':'Serial',
+  'hindi': 'सीरियल',
+};
+Map Qus10={
+  'eng':'What type of test can examine your code as a complete system?',
+  'hindi':'किस प्रकार का परीक्षण एक संपूर्ण सिस्टम के रूप में आपके कोड की जांच कर सकता है?',
+};
+Map Qus10Option1={
+  'eng':'Integration Tests',
+  'hindi':'एकीकरण परीक्षण',
+};
+Map Qus10Option2={
+  'eng':'Unit tests',
+  'hindi':'यूनिट परीक्षण',
+};
+Map Qus10Option3={
+  'eng': 'Widget tests',
+  'hindi': 'विजेट परीक्षण',
+};
+Map Qus10Option4={
+  'eng': 'All of the above',
+  'hindi':'उपरोक्त सभी',
+};
+
+Map You_answer={
+  'eng':'You Answered',
+  'hindi':'आपने उत्तर दिया'
+};
+Map Out_of={
+  'eng': 'out of',
+  'hindi':'से बाहर'
+};
+Map Questions={
+  'eng':'questions correctly',
+  'hindi': 'प्रश्न सही हैं'
+};
+
+Map RESTART={
+  'eng':'Restart Quiz',
+  'hindi':'प्रश्नोत्तरी पुनः प्रारंभ करें',
+};
